62a85facb15fb13992426eaa4f1df5c435906d3c
0300962 <0300962@rgu.ac.uk>
2018-07-01 13:41:49 +0100

    1-7-18 - Task board layout in progress

--- ASDF/task-board.php
+++ ASDF/task-board.php
@@ -6,3 +6,43 @@
  * Time: 12:56
  */
 include 'header.php';
+
+?>
+<link rel="stylesheet" href="CSS/taskboard.css">
+<div id="container">
+    <div id="user_list">
+        <div class="user_box">ABCD</div>
+        <div class="user_box">EFG</div>
+    </div>
+    <div id="task_board">
+        <div class="pbi" id="123">
+            <div class="sbi">
+                <div class="not_started"></div>
+            </div>
+            <div class="sbi">
+                <div class="in_progress"></div>
+            </div>
+            <div class="sbi">
+                <div class="testing"></div>
+            </div>
+            <div class="sbi">
+                <div class="done"></div>
+            </div>
+        </div>
+        <div class="pbi" id="456">
+            <div class="sbi">
+                <div class="not_started"></div>
+            </div>
+            <div class="sbi">
+                <div class="in_progress"></div>
+            </div>
+            <div class="sbi">
+                <div class="testing"></div>
+            </div>
+            <div class="sbi">
+                <div class="done"></div>
+            </div>
+        </div>
+
+    </div>
+</div>
