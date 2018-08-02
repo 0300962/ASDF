0effeb2921469b54ba0a864dd6d2a5c1c91ef70f
0300962 <0300962@rgu.ac.uk>
2018-08-02 14:03:40 +0100

    2-8-18 - Removed connection file from Repo (no use to users).

--- ASDF/Scripts/connection.php
+++
@@ -1,9 +0,0 @@
-<?php  //Created by ASDF at 2018/07/15 02:27:09pm 
-DEFINE ('DB_USER', '0300962'); 
-DEFINE ('DB_PSWD', '0300962'); 
-DEFINE ('DB_HOST', 'localhost'); 
-DEFINE ('DB_NAME', 'asdf'); 
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME); 
-mysqli_select_db($db, DB_NAME); 
-if (!$db) {die('Cannot connect to database');} 
-?>
